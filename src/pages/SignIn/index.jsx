f70edada60e8ec19a4405b96cf2cefb3fe189061
luigi <luigigames2903@gmail.com>
2024-09-24 22:17:30 -0300

    autenticação do usuario

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,19 @@
-import {Container, Form, Background} from "./styles"
-import { Input } from "../../components/input"
-import { Link } from "react-router-dom"
-import { Button } from "../../components/button"
+import { useState } from "react";
+import {Container, Form, Background} from "./styles";
+import { Input } from "../../components/input";
+import { useAuth } from "../../hooks/auth";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/button";
 import {FiUser, FiMail, FiLock} from "react-icons/fi";
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const {signIn} = useAuth();
+
+  function handleSignIn () {
+    signIn({email, password})
+  }
   return (
     <Container>
       <Form>
@@ -12,10 +22,20 @@ export function SignIn() {
 
         <h2>Faça seu Login</h2>
 
-        <Input placeholder="Email" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Link to="/home">
-          <Button title="Entrar" />
+        <Input
+          placeholder="Email"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Link to="">
+          <Button title="Entrar" onClick={handleSignIn} />
         </Link>
 
         <Link to="/cadastrar">Cadastrar</Link>
